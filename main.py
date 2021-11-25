5ffd31e0aab1ff8aafb12c8ab04cdb4b285fcb36
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-11-24 22:26:00 -0500

    Enpint person

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+# Python
+from typing import Optional
+
+#pydantic
+from pydantic import BaseModel
+
+#FastApi
 from fastapi import FastAPI
+from fastapi import Body, Query
+
 
 app = FastAPI()
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+# Request and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
+# validaciones: query parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[int] = Query(...)
+):
+    return {name: age}
